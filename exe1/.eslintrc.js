08454a70211dc29f57f11fb4a0a0f7ae3786bc6b
Miao.Wang2 <Miao.Wang2@lotuscars.com.cn>
2022-07-17 13:20:40 +0800

    feat: 增加项目

---
+++ exe1/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    root: true,
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/attribute-hyphenation': 'off',
+      'vue/no-mutating-props': 'off'
+    }
+  }
---
+++ exe1/src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter,createWebHistory,RouteRecordRaw } from "vue-router";
+import Layout from '@/components/HelloWorld.vue'
+
+// 匹配路由规则
+const routes:Array<RouteRecordRaw>=[
+    {
+        path:'/',
+        name:'home',
+        component:Layout
+    }
+]
+
+// 创建路由
+const router=createRouter({
+    history:createWebHistory(),
+    routes
+})
+
+export default router
